f1c1da089bd49af038ebfa6dda5676a97cbd9100
Unknown <guillaume.d.marceau@gmail.com>
2018-11-05 15:01:00 -0500

    create database

---
+++ create database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE postgres
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'en_US.UTF-8'
+       LC_CTYPE = 'en_US.UTF-8'
+       CONNECTION LIMIT = -1;
+
+COMMENT ON DATABASE postgres
+  IS 'default administrative connection database';
