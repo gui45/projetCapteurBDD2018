2d4dd8ed9e995be3298de46a20db72c4f08967b3
Florian <flolevy33@gmail.com>
2018-11-05 15:58:58 -0500

    insert dans la databas

--- script.py
+++ script.py
@@ -1,16 +1,21 @@
 import serial
-import pgdb
+import psycopg2
 
 adresseServeur = '54.39.144.87'
 utilisateur = 'postgres'
 motDePasse = '9182'
 baseDeDonnees = 'capture'
 
-maConnection = pgdb.connect( host=adresseServeur, user=utilisateur, password=motDePasse, database=baseDeDonnees )
-doQuery( maConnection )
+maConnection = psycopg2.connect(host=adresseServeur, user=utilisateur, password=motDePasse, dbname=baseDeDonnees)
+curseur = maConnection.cursor()
+curseur.execute("INSERT INTO raw (temp) VALUES (54)")
+maConnection.commit()
+
+#doQuery( myConnection )
+
 maConnection.close()
 
-port_serie = "COM3"
+""" port_serie = "COM4"
 baud_rate = 9600
 chemin_fichier = "data.txt"
 
@@ -21,3 +26,7 @@ while True:
     if not (ligne == ""):
         print(ligne.rstrip())
         fichier_sortie.write(ligne.rstrip()+"\n")
+ """
+
+#doQuery( maConnection )
+
