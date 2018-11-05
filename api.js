1b1b52819c44d6a046a551fa0eed50175c4644bf
Antoine <aherkens@gmail.com>
2018-11-05 15:45:24 -0500

    WIP

---
+++ api.js
@@ -0,0 +1,17 @@
+const temperatureDAO = require('./temperatureDAO');
+const http = require('http');
+const url = require('url');
+
+var serveur = http.createServer(
+
+    async function(requete, reponse) {
+
+        if(requete.method === 'GET') {
+            if(requete.url === '/temperature/moyenne' || requete.url === '/temperature/moyenne/') {
+                const moyenne = await temperatureDAO.getMoyenne();
+                reponse.end(JSON.stringify(moyenne));
+            }
+        }
+    }
+);
+serveur.listen(8080);
