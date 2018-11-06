7edf32d371eb2373e0ea7b40586330629fb906a0
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-11-05 19:28:37 -0500

    Proof of concept opérationnelle

--- script.py
+++ script.py
@@ -2,7 +2,9 @@ import serial
 import psycopg2
 import datetime
 import asyncio
+import atexit
 
+### VARIABLES ###
 adresseServeur = "54.39.144.87"
 utilisateur = "postgres"
 motDePasse = "9182"
@@ -10,28 +12,34 @@ baseDeDonnees = "capture"
 
 port_serie = "COM3"
 baud_rate = 9600
-chemin_fichier = "data.txt"
+chemin_fichier = "./data.txt"
 
-maConnection = psycopg2.connect(
-    host=adresseServeur, user=utilisateur, password=motDePasse, dbname=baseDeDonnees)
-curseur = maConnection.cursor()
-
+### FONCTIONS ###
 async def envoyerDonnesEnBDD(valeur):
     curseur.execute(
         "INSERT INTO raw(temp, timestamp) VALUES(%s, %s);", (valeur, datetime.datetime.utcnow()))
+    maConnection.commit()
 
-fichier_sortie = open(chemin_fichier, "w+")
+def fermerConnection():
+    print("Fermeture de la connexion à la BDD.")
+    curseur.close()
+    maConnection.close()
+
+### CODE ###
+atexit.register(fermerConnection)
+
+maConnection = psycopg2.connect(host=adresseServeur, user=utilisateur, password=motDePasse, dbname=baseDeDonnees)
+curseur = maConnection.cursor()
+
+fichier_sortie = open(chemin_fichier, "w")
 serie = serial.Serial(port_serie, baud_rate)
 
-loop = asyncio.get_event_loop()
-loop.run_until_complete(envoyerDonnesEnBDD(25))
+while True:
+    ligne = serie.readline().decode("utf-8")
+    if not (ligne == ""):
+        print(ligne.rstrip() + "\n")
+        fichier_sortie.write(ligne.rstrip() + "\n")
 
-maConnection.commit()
-curseur.close()
-maConnection.close()
+        boucle = asyncio.get_event_loop()
+        boucle.run_until_complete(envoyerDonnesEnBDD(ligne.rstrip()))
 
-# while True:
-#     ligne = serie.readline().decode("utf-8")
-#     if not (ligne == ""):
-#         print(ligne.rstrip() + "\n")
-#         fichier_sortie.write(ligne.rstrip() + "\n")
