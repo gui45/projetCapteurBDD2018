aad05da376fd0a202a7dfb52346dee700ec30ac6
Lucas PARISOT <lucas-parisot@orange.fr>
2018-11-06 21:15:44 -0500

    Application fonctionnelle sous localhost et VM android studio

--- app/src/main/java/ca/qc/cgmatane/informatique/projetcapture/RecevoirMoyenneTemp.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/projetcapture/RecevoirMoyenneTemp.java
@@ -1,9 +1,12 @@
 package ca.qc.cgmatane.informatique.projetcapture;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
+
+import java.io.IOException;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -17,36 +20,55 @@ public class RecevoirMoyenneTemp extends AsyncTask<String, String, String> {
     public RecevoirMoyenneTemp(){}
 
     @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
     protected String doInBackground(String... params) {
 
         Response reponse;
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
-                .url("")
+                .url("http://10.0.2.2:8080/temperature/moyenne/")
                 .build();
+
+        Log.d("LOGTEST","0");
 
         try
         {
+            Log.d("LOGTEST","1");
             reponse = client.newCall(request).execute();
 
+            if (!reponse.isSuccessful())
+                throw new IOException("Code non attendu : " + reponse.toString());
+
+            Log.d("LOGTEST","2");
+
             String jsonDonneesString = reponse.body().string();
-            JSONObject jsonDonneesObjet = new JSONObject(jsonDonneesString);
+            JSONArray jsonArray = new JSONArray(jsonDonneesString);
 
-            String temperatureString = jsonDonneesObjet.getString("temperature");
-            String date = jsonDonneesObjet.getString("timestamp");
+            JSONObject jsonObject = jsonArray.getJSONObject(1);
+            String temperatureString = jsonObject.getString("temp");
+            String date = jsonObject.getString("timestamp");
 
-
-            temperature = new Temperature(temperatureString, date);
+            this.temperature = new Temperature(temperatureString, date);
 
 
 
         } catch(Exception e)
         {
-
+            e.printStackTrace();
+            Log.d("LOGTEST","NON");
         }
 
         return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
     }
 
     public Temperature getTemperature(){
--- app/src/main/java/ca/qc/cgmatane/informatique/projetcapture/VuePrincipale.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/projetcapture/VuePrincipale.java
@@ -2,14 +2,31 @@ package ca.qc.cgmatane.informatique.projetcapture;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.util.concurrent.ExecutionException;
 
 public class VuePrincipale extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activitee_vue_principale);
+
         RecevoirMoyenneTemp recevoirMoyenneTemp = new RecevoirMoyenneTemp();
+        try {
+            recevoirMoyenneTemp.execute().get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         Temperature temperature = recevoirMoyenneTemp.getTemperature();
+
+        Log.d("ALED", "" + recevoirMoyenneTemp.getTemperature());
 
         TextView dateTV = (TextView)findViewById(R.id.date);
         dateTV.setText(temperature.getDate());
@@ -17,7 +34,6 @@ public class VuePrincipale extends AppCompatActivity {
         TextView temperatureTV = (TextView)findViewById(R.id.temperature);
         temperatureTV.setText(temperature.getTemperature());
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activitee_vue_principale);
+
     }
 }
