2d823ca429802a33ef29cdd5820f7c3bcb355b3f
Florian <flolevy33@gmail.com>
2018-11-09 17:14:05 -0500

    Parse liste parametre

---
+++ src/donnees/ParametreDAO.java
@@ -0,0 +1,83 @@
+package donnees;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringBufferInputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import model.Parametre;
+
+public class ParametreDAO {
+
+	String xml = null;
+	
+	public ParametreDAO() {
+		
+	}
+	
+	public Parametre rechercherParametre(){
+		try
+		{
+			URL urlListeParam = new URL("URL A METTRE");
+			String derniereBalise = "</parametres>";
+			InputStream flux = urlListeParam.openConnection().getInputStream();
+			Scanner lecteur = new Scanner(flux);
+			lecteur.useDelimiter(derniereBalise);
+			xml = lecteur.next() + derniereBalise;
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+
+		}
+		try
+		{
+			DocumentBuilder parseur =  DocumentBuilderFactory.newInstance().newDocumentBuilder();
+			@SuppressWarnings("deprecation")
+			Document document = parseur.parse(new StringBufferInputStream(xml));
+			String racine = document.getDocumentElement().getNodeName();
+			System.out.println(racine);
+			
+			NodeList listeNoeudsParametre = document.getElementsByTagName("parametres");
+			for(int position = 0; position < listeNoeudsParametre.getLength(); position++)
+			{
+				Parametre parametre = new Parametre();
+				Element noeudParametres = (Element)listeNoeudsParametre.item(position);
+				String nbHeure = noeudParametres.getElementsByTagName("nbHeure").item(0).getTextContent();
+				parametre.setNbHeure(Float.valueOf(nbHeure));
+				String nbElement = noeudParametres.getElementsByTagName("nbElement").item(0).getTextContent();
+				parametre.setNbElement(Float.valueOf(nbElement));
+				String minParam = noeudParametres.getElementsByTagName("minParam").item(0).getTextContent();
+				parametre.setMinParam(Float.valueOf(minParam));
+				String maxParam = noeudParametres.getElementsByTagName("maxParam").item(0).getTextContent();
+				parametre.setMaxParam(Float.valueOf(maxParam));
+
+
+				return parametre;
+			}
+
+	}catch (ParserConfigurationException e) 
+		{	
+		e.printStackTrace();
+	} catch (SAXException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+		
+		return null;
+}
+}
---
+++ src/model/Parametre.java
@@ -0,0 +1,38 @@
+package model;
+
+public class Parametre {
+	
+	protected float nbHeure;
+	protected float nbElement;
+	protected float minParam;
+	protected float maxParam;
+	
+	
+	public float getNbHeure() {
+		return nbHeure;
+	}
+	public void setNbHeure(float nbHeure) {
+		this.nbHeure = nbHeure;
+	}
+	public float getNbElement() {
+		return nbElement;
+	}
+	public void setNbElement(float nbElement) {
+		this.nbElement = nbElement;
+	}
+	public float getMinParam() {
+		return minParam;
+	}
+	public void setMinParam(float minParam) {
+		this.minParam = minParam;
+	}
+	public float getMaxParam() {
+		return maxParam;
+	}
+	public void setMaxParam(float maxParam) {
+		this.maxParam = maxParam;
+	}
+
+	
+
+}
