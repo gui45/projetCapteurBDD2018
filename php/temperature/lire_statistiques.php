f941986af1a3bf87a8b77173bda2762c94a8feed
Mitsuriou <dylanpro.jacquemin@gmail.com>
2018-11-10 20:07:11 -0500

    Corrections pour l'api de lecture de dernieres statistiques

--- php/temperature/lire_statistiques.php
+++ php/temperature/lire_statistiques.php
@@ -1,51 +1,19 @@
 <?php
-	
 
     include "../connexion.php";
     include "../sql_const.php";
-    
-    /*if (isset($_POST['over']) && isset($_POST['under'])) {
-        $over = $_POST['over'];
-        $under = $_POST['under'];
 
-        if (isset($_POST['intakes'])) {
-            $entrees = $_POST['intakes'];
-            $requete = $basededonnees->prepare(SQL_VALEUR_STATS_INTAKES);
-            $requete->bindParam(":limite", $entrees);
-        }
-        else if (isset($_POST['hours'])) {
-            $heures = $_POST['hours'];
-            //constSQL avec bons param
-            $requete = $basededonnees->prepare(SQL_VALEUR_STATS_HOURS);
-            $requete->bindParam(":limite", $heures);
-        }
-        else {
-            die();
-        }
-    }
-    else {
-        die();
-    }
-
+    $requete = $basededonnees->prepare(SQL_RETOURNER_STATISTIQUES_COMPLETES);
 	$requete->execute();
-    $valeur = $requete->fetchAll(PDO::FETCH_OBJ);*/
-    header('Content-Type: application/xml');
-    echo '<?xml version="1.0" encoding="UTF-8"?>';
+    $valeur = $requete->fetch(PDO::FETCH_OBJ);
+
+    echo "
+        <temperature>
+            <minimum>".$valeur->min."</minimum>
+            <maximum>".$valeur->max."</maximum>
+            <moyenne>".$valeur->moyenne."</moyenne>
+            <mode>".$valeur->mod."</mode>
+            <mediane>".$valeur->med."</mediane>
+        </temperature>
+    ";
 ?>
-<temperature>
-            <minimum>
-                28.4200000762939
-            </minimum>
-            <maximum>
-                28.5
-            </maximum>
-            <moyenne>
-                28.4598425615611
-            </moyenne>
-            <mode>
-                28.5
-            </mode>
-            <mediane>
-                28.4200000762939
-            </mediane>
-</temperature>"
