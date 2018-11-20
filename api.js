6c6160c13bfdb46945dab30bc80938c4d8d4e397
Te-nam <pubmjpub@free.fr>
2018-11-20 12:54:07 -0500

    Amélioration de la structure de l'API

--- api.js
+++ api.js
@@ -1,53 +1,41 @@
 const temperatureDAO = require('./temperatureDAO');
 const http = require('http');
-const url = require('url');
 
 var serveur = http.createServer(
-
-    async function(requete, reponse) {
-
-        if(requete.method === 'GET') {
-            if(requete.url === '/temperature/liste' || requete.url === '/temperature/liste/') {
+    async function(requete, reponse)
+    {
+        if(requete.method === 'GET')
+        {
+            if(requete.url === '/temperature/liste' || requete.url === '/temperature/liste/')
+            {
                 const temperature = await temperatureDAO.listerTemperatures();
                 reponse.end(JSON.stringify(temperature));
             }
-        }
-
-        if(requete.method === 'GET') {
-            if(requete.url === '/temperature/moyenne' || requete.url === '/temperature/moyenne/') {
+            else if(requete.url === '/temperature/moyenne' || requete.url === '/temperature/moyenne/')
+            {
                 const moyenne = await temperatureDAO.moyenneTemperature();
                 reponse.end(JSON.stringify(moyenne));
             }
-        }
-
-        if(requete.method === 'GET') {
-            if(requete.url === '/temperature/mediane' || requete.url === '/temperature/mediane/') {
+            else if(requete.url === '/temperature/mediane' || requete.url === '/temperature/mediane/')
+            {
                 const mediane = await temperatureDAO.medianeTemperature();
                 reponse.end(JSON.stringify(mediane));
             }
-        }
-
-        if(requete.method === 'GET') {
-            if(requete.url === '/temperature/minimum' || requete.url === '/temperature/minimum/') {
+            else if(requete.url === '/temperature/minimum' || requete.url === '/temperature/minimum/')
+            {
                 const minimum = await temperatureDAO.minimumTemperature();
                 reponse.end(JSON.stringify(minimum));
             }
-        }
-
-        if(requete.method === 'GET') {
-            if(requete.url === '/temperature/maximum' || requete.url === '/temperature/maximum/') {
+            else if(requete.url === '/temperature/maximum' || requete.url === '/temperature/maximum/')
+            {
                 const maximum = await temperatureDAO.maximumTemperature();
                 reponse.end(JSON.stringify(maximum));
             }
-        }
-
-        if(requete.method === 'GET') {
-            if(requete.url === '/temperature/mode' || requete.url === '/temperature/mode/') {
+            else if(requete.url === '/temperature/mode' || requete.url === '/temperature/mode/')
+            {
                 const mode = await temperatureDAO.modeTemperature();
                 reponse.end(JSON.stringify(mode));
             }
         }
-    }
-    
-);
+    });
 serveur.listen(8080);
